4713efc44114252b9a7352dc9672d37b2dec01c6
Razvan Sofian <razvan.sofian@yahoo.ro>
2022-03-22 00:13:02 +0200

    small changes to domain
    - added Tile class to GridDomain
    - created Entity module directory

--- Ecosystem/Assets/Domain/Grid/Tile.cs
+++ Ecosystem/Assets/Domain/Grid/Tile.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tile
+namespace GridDomain 
 {
-    private GameObject _tileObject;
-    public Tile(Sprite newSprite)
+    public class Tile
     {
-        this._tileObject = (GameObject) new GameObject();
-        this._tileObject.AddComponent<SpriteRenderer>();
-    }
+        private GameObject _tileObject;
+        public Tile(Sprite newSprite)
+        {
+            this._tileObject = (GameObject) new GameObject();
+            this._tileObject.AddComponent<SpriteRenderer>();
+        }
 
-    public void setSprite(Sprite newSprite)
-    {
-        this._tileObject.GetComponent<SpriteRenderer>().sprite = newSprite;
+        public void setSprite(Sprite newSprite)
+        {
+            this._tileObject.GetComponent<SpriteRenderer>().sprite = newSprite;
+        }
     }
 }
